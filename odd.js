853de9ebc55d650b78eda025480f7c01f1d6fa8d
Kawsar Ahmed <kawsarahmed4148@gmail.com>
2023-03-03 18:49:32 -0800

    odd number 1 to 99 using for loop added

--- odd.js
+++ odd.js
@@ -1,7 +1,14 @@
-// Display 1 to 99 all odd numbers
+// While loop
 
 var i = 1;
 while (i <= 99){
     console.log(i);
     i+=2
+}
+
+
+// for loop
+
+for(var i = 1; i<= 99; i +=2 ){
+    console.log(i)
 }
