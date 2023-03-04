1805ed3d30f206cb406549453e682b9cccf70c55
Kawsar Ahmed <kawsarahmed4148@gmail.com>
2023-03-03 19:24:57 -0800

    for loop with break concept clear

---
+++ break.js
@@ -0,0 +1,15 @@
+for(var i = 0; i<=20; i++){
+    console.log(i)
+    if(i>10){
+        break
+    }
+}
+
+var numbers = [45, 50, 52, 70, 74];
+for (var i = 0; i<numbers.length; i++){
+    const element = numbers[i]
+    if(element == 70){
+        break
+    }
+    console.log(element);
+}
